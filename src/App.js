a76a68fa8b4474cdc9d01e64eaba981e1d8c533a
Fumio Nonaka <fumiononaka@FumioNonaka.local>
2020-09-19 16:08:22 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import CounterDisplay from './CounterDisplay';
 import './App.css';
 
+const initialCount = 0;
 function App() {
-  return (
+	const [count, setCount] = useState(initialCount);
+	const decrement = () => setCount(count - 1);
+	const increment = () => setCount(count + 1);
+	return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CounterDisplay counter={{ count, decrement, increment }} />
     </div>
-  );
+	);
 }
 
 export default App;
