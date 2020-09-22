01f463fa66edaadfa384e04b09e436151cb15919
Fumio Nonaka <fumiononaka@FumioNonaka.local>
2020-09-22 12:26:23 +0900

    Custom hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useCounter } from './useCounter';
 import CounterDisplay from './CounterDisplay';
 import './App.css';
 
-const initialCount = 0;
 function App() {
-	const [count, setCount] = useState(initialCount);
-	const decrement = () => setCount(count - 1);
-	const increment = () => setCount(count + 1);
+	const { count, decrement, increment } = useCounter();
 	return (
     <div className="App">
       <CounterDisplay counter={{ count, decrement, increment }} />
